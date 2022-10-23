2ba0d646ff9f519915139d7413a0e5c958751316
SashaYaroshenko <yaroshenko2019ol@gmail.com>
2022-10-23 19:02:21 +0300

    Add VS project

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,24 @@
+﻿public class Kata
+{
+    public static string FirstNonRepeatingLetter(string s)
+    {
+        char[]charS = s.ToCharArray();
+        char[]charSlower = s.ToLower().ToCharArray();
+        int count = 0;
+        int index = 0;
+        for(int i = 0; i < charSlower.Length; i++)
+        {
+            count = charSlower.Count(x => x == charSlower[i]);
+            if(count==1)
+            {
+                index = i;  
+                break;
+            }
+        }
+        if(count>1)
+        {
+            return "";
+        }
+        return new string(charS[index].ToString());
+    }
+}
